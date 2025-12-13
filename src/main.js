a168ce44011788cc4b9c37629381519278df735b
givanyo <giovanniviana22@gmail.com>
2025-12-13 19:37:45 -0300

    ĺogica da chamada de api

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './style.css';
 
 const usernameInput = document.getElementById('usernameInput');
 const usernameForm = document.getElementById('usernameForm');
-const infoDiv = document.getElementById('info');
+const infoDiv = document.getElementById('infoDiv');
 
 usernameForm.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -11,4 +11,24 @@ usernameForm.addEventListener('submit', (e) => {
   .replaceAll('@', '')
   .replaceAll(' ', '');
   window.alert(username);
+  getInfo(username);
 });
+
+const getInfo = async(username) => {
+  try {
+    const info = await fetch(`https://api.github.com/users/${username}`)
+    .then(response => {
+      if(!response.ok) {
+        throw new Error ('A chamada falhou: Erro ' + response.status)
+      }
+      return response.json()
+    })
+    .then(info => {
+      console.log(JSON.stringify(info));
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+
