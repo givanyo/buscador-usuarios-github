c5211a6710db24ff43e953b83ce28b3c96cce4fa
givanyo <giovanniviana22@gmail.com>
2025-12-13 18:14:28 -0300

    formatacao do input

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-import './style.css'
+import './style.css';
+
+const usernameInput = document.getElementById('usernameInput');
+const usernameForm = document.getElementById('usernameForm');
+const infoDiv = document.getElementById('info');
+
+usernameForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const username = usernameInput.value
+  .trim()
+  .replaceAll('@', '')
+  .replaceAll(' ', '');
+  window.alert(username);
+});
